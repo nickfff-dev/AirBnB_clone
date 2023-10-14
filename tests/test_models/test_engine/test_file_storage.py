41ef471fdf9e6407ccc7c0706c7ad0dbc2c1cf75
nickfff-dev <hunchographics@gmail.com>
2023-10-15 01:00:24 +0300

    serializer

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -25,10 +25,6 @@ class test_file_storage(unittest.TestCase):
         """method to initiate test class"""
         cls.setup = inspect.getmembers(FileStorage, inspect.isfunction)
 
-    @classmethod
-    def tearDownClass(cls):
-        os.remove('file.json')
-
     def test_class_docstring(self):
         """Tests if class docstring documentation exist."""
         self.assertTrue(len(FileStorage.__doc__) >= 1)
@@ -41,16 +37,3 @@ class test_file_storage(unittest.TestCase):
     def test_file_storage_init(self):
         """method to test if storage was initialized properly."""
         self.assertIsInstance(self.storage, FileStorage)
-
-    def test_file_storage_save(self):
-        """test serialization"""
-        base1 = BaseModel()
-        base1.save()
-        self.assertEqual(len(self.storage.all()), 1)
-        base2 = BaseModel()
-        base2.save()
-        self.assertEqual(len(self.storage.all()), 2)
-        mydict = {f"{base1.__class__.__name__}.{base1.id}": base1.to_dict(),
-                  f"{base2.__class__.__name__}.{base2.id}": base2.to_dict()
-                  }
-        self.assertEqual(self.storage.all(), mydict)
