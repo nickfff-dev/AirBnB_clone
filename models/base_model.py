9d17c2503f883fc676ad1ffae5b616a6790def66
nickfff-dev <hunchographics@gmail.com>
2023-10-15 01:12:29 +0300

    serializer

--- models/base_model.py
+++ models/base_model.py
@@ -5,6 +5,7 @@ by all BaseModelInstances.
 """
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel():
@@ -12,37 +13,43 @@ class BaseModel():
     defines a base class BaseModel.
     """
 
-    def __init__(self, id=None, created_at=None, updated_at=None):
+    def __init__(self, *args, **kwargs):
         """initialises a BaseModel instance
-        id(string): a unique identifier generated from uuid.
-        created_at(datetime): the timestamp on creation.
-        updated_at(datetime): the timestamp on update.
+        args(list): wont be used.
+        kwargs: key name pairs of instance attributes.
         """
-        if id is not None:
-            self.id = id
+        if kwargs and len(kwargs):
+            for key in kwargs:
+                if key != '__class__':
+                    if key in ["created_at", "updated_at"]:
+                        setattr(self, key, datetime.strptime(
+                            kwargs[key], "%Y-%m-%dT%H:%M:%S.%f"))
+                    else:
+                        setattr(self, key, kwargs[key])
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self.to_dict())
 
     def __str__(self):
         """"prints the string rep of a class"""
-        return "[" + self.__class__.__name__ + "] " + \
+        return "[" + self.__class__.__name__ + "]" + \
             "(" + self.id + ") " + str(self.__dict__)
 
     def save(self):
-        """method that updates the instance"""
-        self.updated_at = datetime.now()
+        """method that saves the instance to json"""
+        storage.save()
 
     def to_dict(self):
         """
-        method for creating a dict of instance __dict__ keys and values
+        method for creating a dict of instance __dict__ keys and values.
         Returns:
-            dict: a copy of self.__dict__
+            updated_dict(dict): a copy of self.__dict__ .
         """
         updated_dict = {}
         for k, v in self.__dict__.items():
-            if k == 'created_at' or k == 'updated_at':
+            if k in ["created_at", "updated_at"]:
                 updated_dict[k] = datetime.isoformat(v)
             else:
                 updated_dict[k] = v
