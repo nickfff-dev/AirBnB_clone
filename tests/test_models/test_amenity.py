ad9571451f37e89f94993bc426dca8060fb044b9
nickfff-dev <hunchographics@gmail.com>
2023-10-17 20:37:38 +0300

    amenity test

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,8 +1,30 @@
 #!/usr/bin/python3
-"""This module Defines a class for the unittest of class BaseModel
+"""This module Defines a class for the unittest of class Amenity
 """
 import unittest
-from models.base_model import BaseModel
-import inspect
-from datetime import datetime
 from models.amenity import Amenity
+
+
+class TestAmenity(unittest.TestCase):
+    """Test the Amenity class"""
+
+    def setUp(self):
+        """Set up for the tests"""
+        self.amenity = Amenity()
+
+    def tearDown(self):
+        """Tear down for the tests"""
+        del self.amenity
+
+    def test_init(self):
+        """Test the initialization"""
+        self.assertIsInstance(self.amenity, Amenity)
+
+    def test_attributes(self):
+        """Test the attributes"""
+        self.assertTrue(hasattr(self.amenity, 'name'))
+        self.assertEqual(self.amenity.name, "")
+
+
+if __name__ == '__main__':
+    unittest.main()
